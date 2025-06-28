cc3183faa45c38eb503643da60c66de2ee5d5912
Janus Weil <janus@gcc.gnu.org>
2025-06-28 12:28:28 +0200

    Camera.MAUI.Test: start camera preview directly when opening FullScreenPage
    * this exposes the low-resolution problem of issue #40

--- Camera.MAUI.Test/FullScreenPage.xaml.cs
+++ Camera.MAUI.Test/FullScreenPage.xaml.cs
@@ -3,17 +3,19 @@ namespace Camera.MAUI.Test;
 public partial class FullScreenPage : ContentPage
 {
     bool playing = false;
-	public FullScreenPage()
-	{
-		InitializeComponent();
+
+    public FullScreenPage()
+    {
+        InitializeComponent();
         cameraView.CamerasLoaded += CameraView_CamerasLoaded;
     }
+
     private void CameraView_CamerasLoaded(object sender, EventArgs e)
     {
         if (cameraView.Cameras.Count > 0)
         {
             cameraView.Camera = cameraView.Cameras.First();
-            /*
+
             MainThread.BeginInvokeOnMainThread(async () =>
             {
                 if (await cameraView.StartCameraAsync() == CameraResult.Success)
@@ -22,9 +24,10 @@ public partial class FullScreenPage : ContentPage
                     playing = true;
                 }
             });
-            */
+
         }
     }
+
     private async void Button_Clicked(object sender, EventArgs e)
     {
         cameraView.Camera = cameraView.Cameras.First();
